e0b95373e39aeb1b386598254ec8889ada4ed711
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-05-17 17:31:15 -0400

    Stone Chris | Computer Arc One

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -2,6 +2,36 @@
  * LS-8 v2.0 emulator skeleton code
  */
 
+const ADD = 0b10101000;
+const AND = 0b10110011;
+const CALL = 0b01001000;
+const CMP = 0b10100000;
+const DEC = 0b01111001;
+const DIV = 0b10101011;
+const HLT = 0b00000001;
+const INC = 0b01111000;
+const IRET = 0b00001011;
+const JEQ = 0b01010001;
+const JGT = 0b01010100;
+const JLT = 0b01010011;
+const JMP = 0b01010000;
+const JNE = 0b01010010;
+const LD = 0b10011000;
+const LDI = 0b10011001;
+const MOD = 0b10101100;
+const MUL = 0b10101010;
+const NOP = 0b00000000;
+const NOT = 0b01110000;
+const OR = 0b10110001;
+const POP = 0b01001100;
+const PRA = 0b01000010;
+const PRN = 0b01000011;
+const PUSH = 0b01001101;
+const RET = 0b00001001;
+const ST = 0b10011010;
+const SUB = 0b10101001;
+const XOR = 0b10110010;
+
 /**
  * Class for simulating a simple Computer (CPU & memory)
  */
@@ -14,9 +44,10 @@ class CPU {
         this.ram = ram;
 
         this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-        
+        this.reg[7] = 0xf4;
         // Special-purpose registers
         this.PC = 0; // Program Counter
+        this.FL = 0b00000000;
     }
     
     /**
@@ -42,6 +73,91 @@ class CPU {
         clearInterval(this.clock);
     }
 
+    printStack() {
+        console.log("Stack: ", this.ram.read(this.reg[7]));
+    }
+                
+    //Instruction functions
+    and(x, y) {
+        this.reg[x] = this.reg[x] & this.reg[y];
+    }
+    call(register) {
+        this.reg[7]--;
+        this.ram.write(this.reg[7], this.PC + 2);
+          this.PC = this.reg[register];
+        }
+        cmp(regA, regB) {}
+        dec(reg) {
+          this.reg[reg]--;
+        }
+        hlt() {
+          this.stopClock();
+          return;
+        }
+        inc(reg) {
+          this.reg[reg]++;
+        }
+        int(reg) {
+          // Issue the interrupt number stored in the given register.
+          // This will set the _n_th bit in the IS register to the value in the given register.
+        }
+        iret() {
+          // Return from an interrupt handler.
+          // The following steps are executed:
+          // Registers R6-R0 are popped off the stack in that order.
+          // The FL register is popped off the stack.
+          // The return address is popped off the stack and stored in PC.
+          // Interrupts are re-enabled
+        }
+        jeq(reg) {
+          // If equal flag is set (true), jump to the address stored in the given register.
+        }
+        jgt(reg) {
+          // If greater-than flag is set (true), jump to the address stored in the given register.
+        }
+        jlt(reg) {
+          // If less-than flag is set (true), jump to the address stored in the given register.
+        }
+        jmp(reg) {
+          this.PC = reg;
+        }
+        jne(reg) {
+          // If E flag is clear (false, 0), jump to the address stored in the given register.
+        }
+        ld(regA, regB) {
+          //     Loads registerA with the value at the address stored in registerB.
+          // This opcode reads from memory.
+        }
+        ldi(register, integer) {
+          this.reg[register] = integer;
+        }
+        not(reg) {}
+        pop(reg) {
+          // this.printStack();
+          this.reg[reg] = this.ram.read(this.reg[7]);
+          this.ram.write(this.reg[7], null);
+          this.reg[7]          // this.printStack();
+        }
+        pra(reg) {}
+        prn(register) {
+          console.log(this.reg[register]);
+        }
+        push(reg) {
+          // this.printStack();
+          // console.log("Pre SP", this.reg[7]);
+          this.reg[7]--;
+          // console.log("POST SP: ", this.reg[7]);
+          this.ram.write(this.reg[7], this.reg[reg]);
+          // this.printStack();
+        }
+        ret() {
+          this.PC = this.ram.read(this.reg[7]);
+          this.reg[7]++;
+        }
+        st(regA, regB) {
+          this.ram.write(regA, regB);
+        }
+
     /**
      * ALU functionality
      *
@@ -54,9 +170,21 @@ class CPU {
      */
     alu(op, regA, regB) {
         switch (op) {
-            case 'MUL':
-                // !!! IMPLEMENT ME
-                break;
+            case "ADD":
+                this.reg[regA] = this.reg[regA] + this.reg[regB];
+              break;
+            case "DIV":
+              this.reg[regA] = this.reg[regA] / this.reg[regB];
+              break;
+            case "MUL":
+              this.reg[regA] = this.reg[regA] * this.reg[regB];
+              break;
+            case "MOD":
+              this.reg[regA] = this.reg[regA] % this.reg[regB];
+              break;
+            case "SUB":
+              this.reg[regA] -= this.reg[regB];
+              break;
         }
     }
 
@@ -79,10 +207,49 @@ class CPU {
 
         // !!! IMPLEMENT ME
 
+        let next1 = this.ram.read(this.PC + 1);
+        let next2 = this.ram.read(this.PC + 2);
+
         // Execute the instruction. Perform the actions for the instruction as
         // outlined in the LS-8 spec.
 
         // !!! IMPLEMENT ME
+
+        switch (IR) {
+            case ADD:
+            this.alu("ADD", next1, next2);
+            break;
+            case AND:
+            this.and(next1, next2);
+            break;
+            case HLT:
+            this.hlt();
+            break;
+            case LDI:
+            this.ldi(next1, next2);
+            break;
+            case MUL:
+            this.alu("MUL", next1, next2);
+            break;
+            case POP:
+            this.pop(next1);
+            break;
+            case PRN:
+            this.prn(next1);
+            break;
+            case PUSH:
+            this.push(next1);
+            break;
+            case CALL:
+            this.call(next1);
+            break;
+            case RET:
+            this.ret();
+            break;
+            default:
+            console.log("Unknown instruction. Halting");
+            this.hlt();
+        }
 
         // Increment the PC register to go to the next instruction. Instructions
         // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
@@ -90,6 +257,9 @@ class CPU {
         // for any particular instruction.
         
         // !!! IMPLEMENT ME
+        if (IR !== CALL && IR !== RET) {
+            this.PC += (IR >> 6) + 1;
+        }
     }
 }
 
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,8 @@
 const RAM = require('./ram');
 const CPU = require('./cpu');
+const readline = require("readline");
+const fs = require("fs");
+
 
 /**
  * Load an LS8 program into memory
@@ -10,18 +13,31 @@ function loadMemory() {
 
     // Hardcoded program to print the number 8 on the console
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
+    let file = process.argv[2];
+    const filedata = fs.readFileSync(file, "utf8");
+
+    let lines = filedata.trim().split(/[\r\n\s]+/g);
+    lines = lines.filter(line => {
+        if (/^[0-9]{8}$/.test(line)) {
+            return line;
+        }
+    });
+
+    console.log(lines);
+
+    const program = [
+    // print8.ls8
+    "10011001", // LDI R0,8  Store 8 into R0
+    "00000000",
+    "00001000",
+    "01000011", // PRN R0    Print the value in R0
+    "00000000",
+    "00000001" // HLT       Halt and quit
     ];
 
     // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
+    for (let i = 0; i < lines.length; i++) {
+    cpu.poke(i, parseInt(lines[i], 2));
     }
 }
 
